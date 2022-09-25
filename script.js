d98e949313cc0b158aab04be096f5e39138e6d44
Jared Rodriguez <jared7628@gmail.com>
2022-09-24 22:25:53 -0400

    Converted game from console to gui

--- script.js
+++ script.js
@@ -1,112 +1,109 @@
-/**
- * Get player choice
- * 
- * get computer choice
- * 
- * Conditionals to choose winner
- * 
- * loop 5 times while keeping score
- * 
- * After 5 games compare scores to choose overall winner
- * 
- * Ask to replay
- * 
- */
+//Query HTML
+const rockButton = document.querySelector('.rock-button');
+const paperButton = document.querySelector('.paper-button');
+const scissorsButton = document.querySelector('.scissors-button');
+const resultBox = document.querySelector('.result');
+const theChoices = document.querySelector('.choices');
+const resetButton = document.querySelector('.reset');
+const playerScoreBoard = document.querySelector('.player-score');
+const computerScoreBoard = document.querySelector('.computer-score');
+const finalResult = document.querySelector('.final-results');
 
 
-/**This function prompts the player and stores their choice in a variable */
-function getPlayerChoice() {
-    /**While loop returns player choice or reruns if choice is not appropriate */
-    let keepGoing = true
-    while (keepGoing) {
-        let answer = prompt("Rock, Paper, or Scissors?").toLowerCase().trim()
-        if (answer != "rock" && answer != "paper" && answer != "scissors") {
-            console.log("Please type Rock, Paper, or Scissors!")
-            
-        } else {
-            return answer
-            keepGoing = false
-        }
-    }
-}
 
-/**This function returns a random choice of rock, paper, or scissors*/
+//This function returns a random choice of rock, paper, or scissors
 function getComputerChoice() {
-    /**This function defines randomness */
+    //This function defines randomness
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
     }
-    /**Returns choice of rocker paper or scissors */
-    let options = ["rock", "paper", "scissors"]
+    //Returns choice of rocker paper or scissors
+    let options = ["rock", "paper", "scissors"];
     let index = getRandomInt(0, 3);
-    let computerChoice = options[index]
-    return computerChoice
+    let computerChoice = options[index];
+    return computerChoice;
 
 }
 
-/**This defines which choices beat other choices and returns a winner or a lose */
+//This defines which choices beat other choices and returns a win or loss
 function playRound(playerChoice, computerChoice) {
-    let result
+    let result;
+    theChoices.innerText = (`You chose ${playerChoice} and the computer chose ${computerChoice}`);
     if (playerChoice == "rock" && computerChoice == "scissors") {
-        console.log("You won!")
-        result = "win"
-        return result
+        resultBox.innerText = ("You won!");
+        result = "win";
+        return result;
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
-        console.log("You won!")
-        result = "win"
-        return result
+        resultBox.innerText = ("You won!");
+        result = "win";
+        return result;
     } else if (playerChoice == "paper" && computerChoice == "rock") {
-        console.log("You won!")
-        result = "win"
-        return result
+        resultBox.innerText = ("You won!");
+        result = "win";
+        return result;
     } else if (playerChoice == computerChoice) {
-        console.log("It's a Tie")
-        result = "tie"
-        return result
+        resultBox.innerText = ("It's a Tie");
+        result = "tie";
+        return result;
     } else {
-        console.log("You lost!")
-        result = "lose"
-        return result
-    }
-}
+        resultBox.innerText = ("You lost!");
+        result = "lose";
+        return result;
+    };
+};
 
-/**This function plays five rounds and decides a winner based off those rounds */
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-    for (let i = 0; i < 5; i++) {
-        result = playRound(getPlayerChoice(), getComputerChoice());
-        if (result == "win") {
-            playerScore += 1
-        } else if (result == "lose") {
-            computerScore += 1
-        } else {}
-    }
+let playerScore = 0;
+let computerScore = 0;
 
-    console.log(`Your score was ${playerScore} and the computer score was ${computerScore}`)
+//This function keeps score and maintains the best out of five logic
+function keepScore(result) {
+    if (result === "win") {
+        playerScore += 1;
+        playerScoreBoard.innerText = `Your Score: ${playerScore}`;
+    } else if (result === "lose") {
+        computerScore += 1;
+        computerScoreBoard.innerText = `Computer Score: ${computerScore}`;
+    } else {};
+    //This part of the function reads the score each time and decides if the game is over or not
+    if (playerScore + computerScore === 5) {
+        if (playerScore > computerScore) {
+            finalResult.innerText = ("You won best out of 5! Press reset to play again!");
+        } else if (playerScore < computerScore) {
+            finalResult.innerText = ("You lost best out of 5! Press reset to play again!");
+        } else {
+            finalResult.innerText = ("The end result is a tie! Press reset to play again!");
+        };
+    };
+};
 
-    if (playerScore > computerScore) {
-        console.log("You won best out of 5!")
-    } else if (playerScore < computerScore) {
-        console.log("You lost best out of 5!")
-    } else {
-        console.log("The end result is a tie!")
-    }    
-}
 
-/**Call main function named "game()" */
-game();
-/**Play again or cancel */
-while (true) {
-    let playAgain = prompt("Do you want to play again?").toLowerCase().trim();
-    if (playAgain == "yes") {
-        game()
-    } else if (playAgain == "no") {
-        console.log("Ok, see you later!")
-        break
-    } else {
-        console.log("Please type Yes or No")
-    }
-}
+//The three following listeners play a round with the associated button choice
+
+rockButton.addEventListener('click', () => {
+    if (playerScore + computerScore < 5) {
+        let result = playRound("rock", getComputerChoice());
+        keepScore(result);
+    };
+});
+
+paperButton.addEventListener('click', () => {
+    if (playerScore + computerScore < 5) {
+        let result = playRound("paper", getComputerChoice());
+        keepScore(result);
+    };
+});
+
+scissorsButton.addEventListener('click', () => {
+    if (playerScore + computerScore < 5) {
+        let result = playRound("scissors", getComputerChoice());
+        keepScore(result);
+    };
+});
+
+//Reset button refreshes page
+resetButton.addEventListener('click', () => {
+    window.location.reload();
+});
+
